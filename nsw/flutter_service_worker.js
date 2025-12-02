7d51b2a12d7656e333d97c02afe376313acb39df
Richard Lehane <richard@itforarchivists.com>
2025-12-02 20:20:23 +1100

    save / save as

--- nsw/flutter_service_worker.js
+++ nsw/flutter_service_worker.js
@@ -107,14 +107,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "71af41b8865bfde06c34df43225794b9
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "c6eec90483663c5f670a2cceddf968f6",
+"flutter_bootstrap.js": "83722707c66dafe010b9abd104ce1f90",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "ad9a69a0e4bfa89a3ce765a64d73c77d",
 "/": "ad9a69a0e4bfa89a3ce765a64d73c77d",
-"main.dart.js": "6d207d401cb2b9c3f165a2249e43f59a",
+"main.dart.js": "4b21dbfbf5fe7e23fdbca326054ad212",
 "manifest.json": "6a52026f6934836d3b30185e5bad02fa",
 "version.json": "5ae16ef70e230bed08f7490c9ac52224"};
 // The application shell files that are downloaded before a service worker can
